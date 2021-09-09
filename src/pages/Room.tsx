ba9bd97b5a788e3482d8d96c7bb6bb847581bd11
Iann-rst <iannrst11@gmail.com>
2021-09-09 16:19:19 -0300

    Adicionando componente Question na sala visitante

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,6 +5,7 @@ import {useParams} from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
@@ -21,7 +22,7 @@ type FireBaseQuestions = Record<string, {
   isHighLighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string,
@@ -39,7 +40,7 @@ export function Room(){
   const {user} = useAuth();
   const params = useParams<RoomParams>();//pegar o id da pagina, através da URL
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
   const roomId = params.id;
 
@@ -127,7 +128,17 @@ export function Room(){
           </div>
         </form>
         
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map(question =>{
+            return(
+              <Question
+                key={question.id} 
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
