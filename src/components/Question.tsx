e7d7738bf314a40e134a7ecf6d2e405fae9f72cc
Iann-rst <iannrst11@gmail.com>
2021-09-14 10:43:16 -0300

    Adicionado botões de destaque e respondido

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,6 +3,7 @@
  */
 import {ReactNode} from 'react'
 import '../styles/question.scss';
+import cx from 'classnames';
 
 //Tipagem das propriedades que o componente recebe
 type QuestionProps = {
@@ -12,11 +13,26 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighLighted?: boolean;
 }
 
-export function Question({content, author, children}: QuestionProps){
+export function Question({
+  content, 
+  author, 
+  isAnswered = false, 
+  isHighLighted=false, 
+  children
+}: QuestionProps){
   return(
-    <div className="question">
+    <div 
+      className = {cx(
+        'question',
+        {answered: isAnswered}, 
+        {highlighted: isHighLighted && !isAnswered},
+        )}
+    /*{`question ${isAnswered ? 'answered' : ''} ${(isHighLighted && !isAnswered) ? 'highlighted' : ''}`}*/
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
